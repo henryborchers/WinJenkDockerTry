0abede18ccb85e0035b9ba4066dc9f98fda526b5
henryborchers <henryborchers@yahoo.com>
2019-11-08 21:37:32 -0600

    amd 64

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -2,5 +2,5 @@
 set TEST_VAR=something
 echo "hello world"
 REM type c:\BuildTools\Common7\Tools\VsDevCmd.bat
-if not defined DevEnvDir (CALL "c:\BuildTools\Common7\Tools\VsDevCmd.bat" -help)
+if not defined DevEnvDir (CALL "c:\BuildTools\Common7\Tools\VsDevCmd.bat" -arch amd64)
 dir
