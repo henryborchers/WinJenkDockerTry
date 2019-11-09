81d4aad5bdd4c527bf710cf0c0270337efda55b6
henryborchers <henryborchers@yahoo.com>
2019-11-08 21:28:01 -0600

    cd

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -2,6 +2,7 @@
 set TEST_VAR=something
 echo "hello world"
 REM type c:\BuildTools\Common7\Tools\VsDevCmd.bat
-CALL "c:\BuildTools\Common7\Tools\VsDevCmd.bat /?"
+cd c:\BuildTools\Common7\Tools
+CALL "VsDevCmd.bat"
 REM if not defined DevEnvDir (
 REM )
