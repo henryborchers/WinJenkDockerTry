226af9745cda266f822b4cf556070423d362ec3a
henryborchers <henryborchers@yahoo.com>
2019-11-08 22:41:32 -0600

    test

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -1,2 +1,2 @@
 @echo off
-if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext -arch=amd64 -host_arch=amd64)
+if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext -arch=amd64 -host_arch=amd64 -test)
