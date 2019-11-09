2ae355aa5993a45e3358e36d97ce59e0fc876f17
henryborchers <henryborchers@yahoo.com>
2019-11-08 21:24:29 -0600

    quotes

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -1,8 +1,7 @@
 @echo off
 set TEST_VAR=something
 echo "hello world"
-dir c:\BuildTools\Common7\Tools
 REM type c:\BuildTools\Common7\Tools\VsDevCmd.bat
 if not defined DevEnvDir (
-    call c:\BuildTools\Common7\Tools\VsDevCmd.bat /?
+    CALL "c:\BuildTools\Common7\Tools\VsDevCmd.bat"
 )
