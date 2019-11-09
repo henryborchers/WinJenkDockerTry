d5bd6d41304052596234540349b5cb2ee36e4085
henryborchers <henryborchers@yahoo.com>
2019-11-08 22:34:16 -0600

    clean up

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -1,6 +1,2 @@
-REM @echo off
-set TEST_VAR=something
-echo "hello world"
-type c:\BuildTools\Common7\Tools\VsDevCmd.bat
-REM set
-if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext)
+@echo off
+if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext -no_logo)
