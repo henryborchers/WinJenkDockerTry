f74f0d32774fb6bf4a36064f136d74ed2ffe39ae
henryborchers <henryborchers@yahoo.com>
2019-11-08 23:22:49 -0600

    debug

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -1,3 +1,6 @@
 REM @echo off
-if not defined DevEnvDir (CALL C:\BuildTools\Common7\Tools\VsDevCmd.bat  -arch=amd64)
+if not defined DevEnvDir (
+    set VSCMD_DEBUG=2
+    CALL C:\BuildTools\Common7\Tools\VsDevCmd.bat  -arch=amd64
+    )
 set
