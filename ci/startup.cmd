78cc59098d2a9125640748dac492ab3c62837732
henryborchers <henryborchers@yahoo.com>
2019-11-08 22:27:20 -0600

    single quotes

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -3,4 +3,4 @@ set TEST_VAR=something
 echo "hello world"
 REM type c:\BuildTools\Common7\Tools\VsDevCmd.bat
 set
-if not defined DevEnvDir (CALL "c:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_logo)
+if not defined DevEnvDir (CALL 'c:\BuildTools\Common7\Tools\VsDevCmd.bat' -no_logo)
