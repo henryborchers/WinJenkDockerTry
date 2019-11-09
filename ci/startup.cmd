b5a5124f345d5a6a4a1b74f83cd118d98b71650e
henryborchers <henryborchers@yahoo.com>
2019-11-08 23:13:54 -0600

    no test

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -1,2 +1,2 @@
 REM @echo off
-if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext -arch=amd64 -host_arch=amd64 -test)
+if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext -arch=amd64)
