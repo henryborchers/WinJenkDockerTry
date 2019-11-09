85d5041d1bdebc746790f0fb339732a42c64cbfc
henryborchers <henryborchers@yahoo.com>
2019-11-08 22:36:55 -0600

    logo

--- ci/startup.cmd
+++ ci/startup.cmd
@@ -1,2 +1,2 @@
 @echo off
-if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext -no_logo)
+if not defined DevEnvDir (CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" -no_ext)
